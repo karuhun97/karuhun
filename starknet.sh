1888296b7d3bb3ca01992fb2ee2c648bd03a95cd
Karuhun <138513513+karuhun97@users.noreply.github.com>
2024-05-19 08:29:44 +0700

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-91a49a43-43b9-441d-92ea-a39addd9f949/"
 
 OUTPUT_DIR="logs"
 
